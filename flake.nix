5fb05c09e0377fa45e67be8bf4568bf0f64ca0af
Joshua Newman <josh@d8e.ai>
2023-09-19 11:26:11 -0700

    use flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      rec {
+        packages.pkgs = pkgs;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs
+          ];
+        };
+      }
+    );
+}
